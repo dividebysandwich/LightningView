39466f0627f061297c6fb19ce29cb8ed33f301c6
dividebysandwich <josef.jahn@gmail.com>
2025-10-07 12:56:50 +0200

    Version 2.2.0

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ const TILE_SIZE: usize = 1024; // Use tiles of 1024x1024 pixels for the detail v
 pub const IMAGEREADER_SUPPORTED_FORMATS: [&str; 4] = ["webp", "tif", "tiff", "tga"];
 pub const ANIM_SUPPORTED_FORMATS: [&str; 1] = ["gif"];
 pub const IMAGE_RS_SUPPORTED_FORMATS: [&str; 9] = ["jpg", "jpeg", "png", "bmp", "svg", "ico", "pnm", "xbm", "xpm"];
-pub const RAW_SUPPORTED_FORMATS: [&str; 23] = ["mrw", "arw", "srf", "sr2", "nef", "mef", "orf", "srw", "erf", "kdc", "dcs", "rw2", "raf", "dcr", "dng", "pef", "crw", "iiq", "3fr", "nrw", "mos", "cr2", "ari"];
+pub const RAW_SUPPORTED_FORMATS: [&str; 24] = ["mrw", "arw", "srf", "sr2", "nef", "mef", "orf", "srw", "erf", "kdc", "dcs", "rw2", "raf", "dcr", "dng", "pef", "crw", "iiq", "3fr", "nrw", "mos", "cr2", "ari", "ori"];
 pub const FITS_SUPPORTED_FORMATS: [&str; 2] = ["fits", "fit"];
 
 // --- Advanced Data Structures for Tiled Viewing ---
